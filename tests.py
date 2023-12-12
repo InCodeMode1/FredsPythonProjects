11a2c1ee5041c0c4374c6f7aa3473ee2b3d9dd0b
Alfredo Boron <alron1984@gmail.com>
2023-12-11 21:31:38 -0800

    added unit test for reset_cells_visited

--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
 import unittest
 from maze_solver import Maze, Point, Window
 
+
 class Tests(unittest.TestCase):
+
     def test_maze_create_cells(self):
-        num_cols = 12
-        num_rows = 10
-        m1 = Maze(Point(0, 0), num_rows, num_cols, Point(10, 10), Window(800, 600))
+        num_cols = 7
+        num_rows = 7
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -14,6 +16,27 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 10
+        num_rows = 10
+        m1 = Maze(0, 0,  num_rows, num_cols, 0, 0)
+        self.assertEqual(
+            m1._cells[0][0].has_left_wall,
+            False,
+        )
+        self.assertEqual(
+            m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            False,
+        )
+
+    def test_reset_cells_visited(self):
+        num_cols = 10
+        num_rows = 10
+        m1 = Maze(0, 0,  num_rows, num_cols, 0, 0)
+        for i in range(m1._num_cols):
+            for j in range(m1._num_rows):
+                self.assertEqual(m1._cells[i][j].visited, False)
 
 
 if __name__ == '__main__':
